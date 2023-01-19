e99ec1afa5a9d40c84d6671e007d79d9081589bb
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2023-01-20 00:00:36 +0530

    [Nehal] Adding few numbers in the linked list in sequentially

--- Llist/Program.cs
+++ Llist/Program.cs
@@ -7,9 +7,9 @@ namespace Llist
         static void Main(string[] args)
         {
             Linkedlist a = new Linkedlist();
-            a.add(56);
-            a.add(30);
             a.add(70);
+            a.add(30);
+            a.add(56);
             a.show();
         }
     }
