f935ba08db2bb8ff405c1932b57651c82ab9f2e4
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2023-01-19 23:19:07 +0530

    [Nehal] Adding few numbers in the linked list

---
+++ Llist/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Llist
+{
+    internal class Node
+    {
+        public int data;
+        public Node next;
+
+    }
+}
---
+++ Llist/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Llist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Linkedlist a = new Linkedlist();
+            a.add(56);
+            a.add(30);
+            a.add(70);
+            a.show();
+        }
+    }
+}
